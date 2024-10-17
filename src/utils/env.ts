871b27ef112af726e5f8ac806232aaf462dcd610
Kauan Curbani <87383246+KauanCurbani@users.noreply.github.com>
2024-10-17 16:39:59 -0300

    populate config

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   JWT_SECRET: z.string(),
   DATABASE_URL: z.string(),
-  POPULATE_DATABASE: z.boolean().default(false),
+  POPULATE_DATABASE: z.string().optional(),
 });
 
 export type Env = z.infer<typeof envSchema>;
